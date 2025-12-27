7e05baae424be1e4600cc38915fbf4428b4a85e6
mae <ame>
2025-12-27 22:21:59 +0100

    correction key modifier / bluetooth mode

--- main/comm/hid_bluetooth_manager.h
+++ main/comm/hid_bluetooth_manager.h
@@ -5,7 +5,7 @@
 
 extern void init_hid_bluetooth(void);
 extern void deinit_hid_bluetooth(void);
-extern void send_hid_bl_key(uint8_t keycodes[6]);
+extern void send_hid_bl_key(uint8_t modifier, uint8_t keycodes[6]);
 extern void send_hid_bl_mouse(uint8_t buttons, int8_t x, int8_t y, int8_t wheel);
 extern bool hid_bluetooth_is_initialized(void);
 extern bool hid_bluetooth_is_connected(void);
